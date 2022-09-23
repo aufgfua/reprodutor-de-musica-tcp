50684429ce97dc5688abd39c03e6961b19a3b662
Augusto Flach <augustofalcaoflach@gmail.com>
2022-09-22 23:52:51 -0300

    create Controller class

---
+++ src/main/java/br/ufrgs/inf/tcp/trabalho/Controller.java
@@ -0,0 +1,20 @@
+package br.ufrgs.inf.tcp.trabalho;
+
+public class Controller {
+
+    private String textReader;
+    private String player;
+    private String lastNote;
+
+
+    public void processaTexto(){
+        
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+}
--- src/main/java/br/ufrgs/inf/tcp/trabalho/Main.java
+++
@@ -1,7 +0,0 @@
-package br.ufrgs.inf.tcp.trabalho;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
